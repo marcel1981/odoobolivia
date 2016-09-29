3138ae959f01ae06f8ba3df9a2495f256dd47dcc
root <ivanmcaa@hotmail.com>
2016-09-29 12:34:40 -0400

    Module Update Description

--- partner_dni_bo/__openerp__.py
+++ partner_dni_bo/__openerp__.py
@@ -27,11 +27,46 @@
         """,
 
     'description': """
-        English: This module was developed to add identity card and identification for Department where Bolivia was issued fields, Bolivia also establishes default when registering a client.
-        
-        Spanish: Este modulo se desarrollo para añadir los campos Carnet de Identidad y Departamento donde se emitio la identificacion para Bolivia, tambien establece por defecto Bolivia al momento de registrar un cliente.
-        
-        Restriccion: C.I. y Departamento son un identificador unico: _sql_constraints = [('ci_emision_unique', 'unique(ci,emision), 'Error: Customer already registered!')].
+        English
+
+        This module was developed to add identity card and identification for Department where Bolivia was issued fields, Bolivia also establishes default when registering a client.
+
+
+        UNIQUE Restriccion
+
+        C.I. and the Department are unique identifier:
+
+        _sql_constraints = [('ci_emision_unique','UNIQUE (ci,emission)','Error: Customer already registered')]
+
+        DEFAULT Setting
+
+        So that the user does not register at the point of sale the same data of the region, it is set by default the most common users.
+
+        'Country' = 'Bolivia'
+        'Status =' La Paz '
+        'Emission' = 'La Paz'
+        'City' = 'El Alto'
+        'Zip' = 'LP'
+
+        SPANISH
+
+        Este modulo se desarrollo para añadir los campos Carnet de Identidad y Departamento donde se emitio la identificacion para Bolivia, tambien establece por defecto Bolivia al momento de registrar un cliente.
+
+        Restriccion UNICO
+
+        C.I. y Departamento son un identificador unico: 
+
+        _sql_constraints = [('ci_emision_unique', 'UNIQUE(ci,emision), 'Error: Cliente ya registrado!')].
+
+        Estableciendo valores por DEFECTO
+
+        Para que el usuario no tenga que registrar en el punto de venta los mismos datos de la region, se establece por defecto los usuarios mas comunes.
+
+        'Pais' = 'Bolivia',
+        'Estado' = 'La Paz',
+        'Emision' = 'La Paz',
+        'Ciudad' = 'El Alto',
+        'Zip' = 'LP',
     """,
 
     'author': "Ivan Carvajal",
--- partner_dni_bo/controllers/controllers.py
+++ partner_dni_bo/controllers/controllers.py
@@ -19,21 +19,3 @@
 #
 ##############################################################################
 from openerp import http
-
-# class PartnerDniBo(http.Controller):
-#     @http.route('/partner_dni_bo/partner_dni_bo/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/partner_dni_bo/partner_dni_bo/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('partner_dni_bo.listing', {
-#             'root': '/partner_dni_bo/partner_dni_bo',
-#             'objects': http.request.env['partner_dni_bo.partner_dni_bo'].search([]),
-#         })
-
-#     @http.route('/partner_dni_bo/partner_dni_bo/objects/<model("partner_dni_bo.partner_dni_bo"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('partner_dni_bo.object', {
-#             'object': obj
-#         })
