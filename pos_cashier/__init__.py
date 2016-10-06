72faf4c475ee113d6375a7c53b598547db97ff33
root <ivanmcaa@hotmail.com>
2016-10-06 14:56:55 -0400

    Delete modules error

--- pos_cashier/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Slavishly copied from Thierry Godin tutorial on the web
-# http://thierry-godin.developpez.com/openerp/tutorial-module-creation-pos-modification-english-version/
-#
-
-import pos_cashier
